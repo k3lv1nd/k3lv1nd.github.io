2dc0dadb9823496ff42a89316bd17573f8937fb9
kelvin <kalvind95@gmail.com>
2021-01-02 23:41:25 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CodeNerve.github.io
-Terminal based portfolio website for CodeNerve
+# k3lv1nd.github.io
+My simple portfolio
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Typer = {
   text: '',
   accessCountimer: null,
   index: 0,
-  speed: 2,
+  speed: 1,
   file: '',
   accessCount: 0,
   deniedCount: 0,
@@ -96,7 +96,7 @@ function replaceUrls(text) {
 }
 
 Typer.speed = 3;
-Typer.file = 'CodeNerve.txt';
+Typer.file = 'k3lv1nd.txt';
 Typer.init();
 
 var timer = setInterval('t();', 30);
